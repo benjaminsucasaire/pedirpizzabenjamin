30c3c97baba2244cab06be4e83bb0ec8d5ac5ae6
benjaminsucasaire <bsucasaire@applying.pe>
2022-06-18 17:01:06 -0500

    ADD: se agregó SQS

--- handler.js
+++ handler.js
@@ -1,18 +1,43 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const { v4: uuidv4 } = require('uuid');
+const AWS = require('aws-sdk');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+var sqs = new AWS.SQS({region: process.env.REGION});
+const QUEUE_URL = process.env.PENDING_ORDER_QUEUE;
+
+module.exports.hacerPedido = (event, context, callback) => {
+	console.log('HacerPedido fue llamada');
+	const orderId = uuidv4();
+
+	const params = {
+		MessageBody: JSON.stringify({ orderId: orderId }),
+		QueueUrl: QUEUE_URL
+	};
+
+	sqs.sendMessage(params, function(err, data) {
+		if (err) {
+			sendResponse(500, err, callback);
+		} else {
+			const message = {
+				orderId: orderId,
+				messageId: data.MessageId
+			};
+			sendResponse(200, message, callback);
+		}
+	});
 };
+
+module.exports.prepararPedido = (event, context, callback)=>{
+	console.log('prepararPedido fue llamado');
+	console.log(event);
+	callback();
+};
+
+function sendResponse(statusCode, message, callback) {
+	const response = {
+		statusCode: statusCode,
+		body: JSON.stringify(message)
+	};
+	callback(null, response);
+}
